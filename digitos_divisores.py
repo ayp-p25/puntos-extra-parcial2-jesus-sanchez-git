6ba0b4aab4029e0057061a6d9954ce6a75a7d62c
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-28 20:28:35 -0600

    First

--- digitos_divisores.py
+++ digitos_divisores.py
@@ -1,1 +1,8 @@
-# Codifica aquí tu programa
+C = 0
+N = int(input("Ingrese un numero : \n"))
+for i in str(N):
+    d = int(i)
+    if d != 0 and N % d == 0:
+        C += 1
+
+print(f"El numero {N} tiene estos divisores {C}")
